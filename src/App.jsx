208089247c3a666abe8b6dd37d0b5d47e064709e
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-10-17 19:07:51 +0600

    user added to the console

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,33 @@ function App() {
       .then(data => setUsers(data));
   }, [])
 
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const salary = form.salary.value;
+    console.log(name, salary);
+
+  }
+
+
   return (
     <>
       <div>
         <h1>User Management System</h1>
         <h3>Number of users: {users.length}</h3>
+        <form onSubmit={handleAddUser}>
+          <input type="text" name="name" id="" />
+          <br />
+          <input type="number" name="salary" id="" />
+          <br />
+          <input type="submit" value="Add User" />
+        </form>
+        <div>
+          {
+            users.map(users => <p key={users.id}>{users.id} {users.name} {users.salary}</p>)
+          }
+        </div>
 
       </div>
 
